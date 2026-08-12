07e4b13763923d8bd08e76bf22d0d6d32178b47c
Joao Figueiredo <joaonvfigueiredo@gmail.com>
2026-08-12 17:04:24 +0100

    initial commit.

---
+++ lib/apex2.ex
@@ -0,0 +1,18 @@
+defmodule Apex2 do
+  @moduledoc """
+  Documentation for `Apex2`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Apex2.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/apex2_test.exs
@@ -0,0 +1,8 @@
+defmodule Apex2Test do
+  use ExUnit.Case
+  doctest Apex2
+
+  test "greets the world" do
+    assert Apex2.hello() == :world
+  end
+end
